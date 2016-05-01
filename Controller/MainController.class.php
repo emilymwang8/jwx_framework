3b95c28ca8f6b163b094d1af23c938794a13ef68
0x584A <xjiek2010@icloud.com>
2016-05-02 03:16:47 +0800

    初始后台系统管理模板页面

---
+++ Controller/MainController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* JWX后台管理控制器
+*/
+class MainController extends Controller
+{
+    // 加载显示首页模板
+    public function index()
+    {
+        // 显示模板页面
+        $this->view->display('Main/index.tpl');
+    }
+}
